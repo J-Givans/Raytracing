0841bebc530be482354c7180b3de044c8c831ed6
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-08 19:53:36 +0300

    Feature: write a series of pixel colours to stdout using Colour and Vec3

--- main.cpp
+++ main.cpp
@@ -1,31 +1,26 @@
+#include "Colour.hpp"
+#include "Vec3.hpp"
+
 #include <iostream>
-#include <array>
-#include <numeric>
 
 int main()
 {
+    // Image
     static constexpr int imgWidth {256};
     static constexpr int imgHeight {256};
 
+    // Render
     std::cout << "P3\n" << imgWidth << ' ' << imgHeight << "\n255\n";
 
     for (auto j = imgHeight - 1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << '\n' << std::flush;
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 
         for (auto i = 0; i < imgWidth; ++i) {
-            auto r = static_cast<double>(i) / (imgWidth - 1);
-            auto g = static_cast<double>(j) / (imgHeight - 1);
-            static constexpr double b {0.25};
-            
-            auto ir = static_cast<int>(255.999 * r);
-            auto ig = static_cast<int>(255.999 * g);
-            auto ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            Colour pxColour((static_cast<double>(i) / imgWidth - 1), (static_cast<double>(j) / imgHeight - 1), 0.25);
+            writeColour(std::cout, pxColour);
         }
     }
 
-    std::cerr << "Done.\n";
-
+    std::cerr << "\nDone.\n";
     return EXIT_SUCCESS;
 }
