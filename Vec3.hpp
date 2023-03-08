ec19da88686094b93975a25b0d20e9723d485799
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-08 19:16:12 +0300

    Feature: create a new vector from difference of 2 others

--- Vec3.hpp
+++ Vec3.hpp
@@ -116,4 +116,11 @@ constexpr Vec3 operator+(Vec3 const& u, Vec3 const& v) noexcept
     return Vec3(u[0] + v[0], u[1] + v[1], u[2] + v[2]);
 }
 
+/// \brief Create a new vector from the difference of two other vectors
+/// \returns A new vector whose coordinates are the differences of @param u and @param v coordinates
+constexpr Vec3 operator-(Vec3 const& u, Vec3 const& v) noexcept
+{
+    return Vec3(u[0] - v[0],u[1] - v[1], u[2] - v[2]);
+}
+
 #endif
