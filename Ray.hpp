f6fc248ab1a5c1a859c053099f47a7cf36e2886d
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-08 19:58:14 +0300

    Feature: initial commit
    Constructors and member variable definitions

---
+++ Ray.hpp
@@ -0,0 +1,28 @@
+#ifndef RAY_HPP
+#define RAY_HPP
+
+#include "Vec3.hpp"
+
+/// \brief 3d point
+using Point3 = Vec3;
+
+class Ray
+{
+public:
+    /// \brief Default constructor
+    constexpr Ray() noexcept = default;
+
+    /// \brief Construct a ray given its origin and direction of travel
+    /// \param[in] origin The origin of the ray
+    /// \param[in] direction The direction towards which the ray is travelling
+    constexpr Ray(Point3 const& origin, Vec3 const& direction) noexcept
+    :   m_origin(origin), m_direction(direction)
+    {
+    }
+
+private:
+    Point3 m_origin;
+    Vec3 m_direction;
+};
+
+#endif
