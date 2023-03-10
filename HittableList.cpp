19cd096014e3367c2d3c87ced09bdb70c3fa0243
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-10 16:28:28 +0300

    Feature: create a list of hittable objects

---
+++ HittableList.cpp
@@ -0,0 +1,33 @@
+#include "HittableList.hpp"
+
+HittableList::HittableList(std::shared_ptr<Hittable> object) : HittableList()
+{
+    add(object);
+}
+
+void HittableList::clear()
+{
+    m_objects.clear();
+}
+
+void HittableList::add(std::shared_ptr<Hittable> object)
+{
+    m_objects.emplace_back(object);
+}
+
+bool HittableList::hit(Ray const& ray, double tMin, double tMax, HitRecord& record) const noexcept
+{
+    HitRecord temp;
+    bool hitAnything = false;
+    auto closestSoFar = tMax;
+
+    for (auto const& obj : m_objects) {
+        if (obj->hit(ray, tMin, closestSoFar, temp)) {
+            hitAnything = true;
+            closestSoFar = temp.t;
+            record = temp;
+        }
+    }
+
+    return hitAnything;
+}
---
+++ HittableList.hpp
@@ -0,0 +1,34 @@
+#ifndef HITTABLE_LIST_HPP
+#define HITTABLE_LIST_HPP
+
+#include "Hittable.hpp"
+
+#include <memory>
+#include <vector>
+
+class HittableList : public Hittable
+{
+public:
+    /// \brief Default constructor
+    HittableList() = default;
+
+    /// \brief Constructor. 
+    ///     Adds a Hittable object to the list of Hittable objects
+    /// \param[in] object A shared pointer to a Hittable object
+    explicit HittableList(std::shared_ptr<Hittable> object);
+
+    /// \brief Erases the list of Hittable objects
+    void clear();
+
+    /// \brief Add an object to the list of Hittable objects
+    /// \param[in] object A shared pointer to a Hittable object
+    void add(std::shared_ptr<Hittable> object);
+
+    /// \brief Determine if a ray hit an object
+    bool hit(Ray const& ray, double tMin, double tMax, HitRecord& record) const noexcept override;
+
+private:
+    std::vector<std::shared_ptr<Hittable>> m_objects;
+};
+
+#endif
