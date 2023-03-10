375c2b5aa06a91e0fa19fb5b14817e59dfdb0a9a
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-10 16:29:39 +0300

    Feature: define common utilities and headers

---
+++ Common.hpp
@@ -0,0 +1,19 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include "Ray.hpp"
+#include "Vec3.hpp"
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+inline constexpr double infinity = std::numeric_limits<double>::infinity();
+inline constexpr double pi = 3.1415926535897932385;
+
+constexpr double degreesToRadians(double const degrees) noexcept
+{
+    return degrees * pi / 180.0;
+}
+
+#endif
