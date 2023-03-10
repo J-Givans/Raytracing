49c0093ca4b1ea366647eebc112c0f01d3f0abe3
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-10 16:15:19 +0300

    Feature: determine the direction the normal is pointing towards

--- Hittable.hpp
+++ Hittable.hpp
@@ -9,7 +9,28 @@ struct HitRecord
     Point3 point;
     Vec3 normal;
     double t;
+    bool frontFace;     // In which direction is the normal pointing towards from the surface?
+
+    /// \brief Set the frontFace and the normal
+    /// \param[in] ray The incidence ray
+    /// \param[in] outwardNormal The normal at the point of ray-sphere intersection
+    constexpr void setFaceNormal(Ray const& ray, Vec3 const& outwardNormal) & noexcept;
 };
+
+constexpr void HitRecord::setFaceNormal(Ray const& ray, Vec3 const& outwardNormal) & noexcept
+{
+    auto const setFrontFace = [&ray, &outwardNormal]() noexcept {
+        if (dot(ray.getDirection(), outwardNormal) < 0) {
+            return false;
+        }
+
+        return true;
+    };
+
+    frontFace = setFrontFace();
+
+    normal = frontFace ? outwardNormal : -outwardNormal;
+}
 
 class Hittable
 {
