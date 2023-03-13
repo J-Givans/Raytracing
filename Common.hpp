cef73c5abe5bc5aabac0f799cf09f4208bb57697
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-13 20:00:51 +0300

    Refactor: remove unused header

--- Common.hpp
+++ Common.hpp
@@ -6,7 +6,6 @@
 
 #include <cmath>
 #include <limits>
-#include <memory>
 
 inline constexpr double infinity = std::numeric_limits<double>::infinity();
 inline constexpr double pi = 3.1415926535897932385;
