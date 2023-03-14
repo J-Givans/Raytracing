62838e6b276e218ba22a8761e8f7ff6a46bfe5d7
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-03-14 14:38:32 +0300

    Feature: implement multi-sampling for the writeColour function

--- src/Colour.cpp
+++ src/Colour.cpp
@@ -1,9 +1,17 @@
 #include "Colour.hpp"
+#include "Common.hpp"
 
-void writeColour(std::ostream& out, Colour const& pixelColour)
+void writeColour(std::ostream& out, Colour const& pixelColour, int const samplesPerPixel)
 {
+    // Divide the colour by the number of pixels
+    double const scale = 1.0 / samplesPerPixel;
+
+    double const r = pixelColour.x() * scale;
+    double const g = pixelColour.y() * scale;
+    double const b = pixelColour.z() * scale;
+
     // Write the translated [0, 255] value of each colour component
-    out << static_cast<int>(255.999 * pixelColour.x()) << ' '
-        << static_cast<int>(255.999 * pixelColour.y()) << ' '
-        << static_cast<int>(255.999 * pixelColour.z()) << '\n';
+    out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(255.999 * clamp(b, 0.0, 0.999)) << '\n';
 }
--- src/Colour.hpp
+++ src/Colour.hpp
@@ -12,6 +12,7 @@ using Colour = Vec3;
 /// \brief Write a single pixel's colour to the standard output stream
 /// \param[in] pixelColour The pixel whose colour is being written
 /// \param[inout] out The output stream written to
-void writeColour(std::ostream& out, Colour const& pixelColour);
+/// \param[in] samplesPerPixel The number of samples of each pixel
+void writeColour(std::ostream& out, Colour const& pixelColour, int const samplesPerPixel);
 
 #endif
