00090973303888ae8f89f0635a7ba43fb0881ae6
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-04-30 12:02:38 +0300

    Test - add tests for Vec3 accessor methods

--- tests/Vec3.test.cpp
+++ tests/Vec3.test.cpp
@@ -15,3 +15,18 @@ TEST_F(Vec3Test, IsAllZerosWhenCreated)
         ASSERT_TRUE(vec_[i] == 0);
     }
 }
+
+TEST_F(Vec3Test, MethodXReturnsFirstValue)
+{
+    ASSERT_FLOAT_EQ(vec_.x(), vec_[0]);
+}
+
+TEST_F(Vec3Test, MethodYReturnsSecondValue)
+{
+    ASSERT_FLOAT_EQ(vec_.y(), vec_[1]);
+}
+
+TEST_F(Vec3Test, MethodZReturnsLastValue)
+{
+    ASSERT_FLOAT_EQ(vec_.z(), vec_[2]);
+}
