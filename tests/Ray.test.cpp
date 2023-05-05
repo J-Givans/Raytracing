06ac4e2ca0450552d006e3b5e9d2a80f71010288
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-05-05 17:02:52 +0300

    Test - add test case for the default constructor
    - Define a function that tests two Vec3's for equality
    - Define a function that tests two Rays for equality
    - We use these functions to assert that a default constructed Ray is equal to the Vec3(0, 0, 0)

--- tests/Ray.test.cpp
+++ tests/Ray.test.cpp
@@ -2,3 +2,37 @@
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <gtest/gtest_pred_impl.h>
+
+using namespace rt;
+using namespace ::testing;
+
+[[nodiscard]] 
+testing::AssertionResult operator==(Vec3 const& u, Vec3 const& v) noexcept
+{
+    if (u.x() == v.x() and u.y() == v.y() and u.z() == v.z()) {
+        return testing::AssertionSuccess();
+    }
+    else {
+        return testing::AssertionFailure() << " are unequal.";
+    }
+}
+
+[[nodiscard]]
+testing::AssertionResult operator==(Ray const& r, Ray const& t) noexcept
+{
+    if (r.getOrigin() == t.getOrigin() and r.getDirection() == t.getDirection()) {
+        return testing::AssertionSuccess();
+    }
+    else {
+        return testing::AssertionFailure() << " are unequal.";
+    }
+}
+
+TEST(RayTest, IsPositionedAtOriginWithZeroDirectionVectorByDefault)
+{
+    constexpr Ray ray;
+    
+    ASSERT_TRUE(ray.getOrigin() == Vec3(0, 0, 0));
+    ASSERT_TRUE(ray.getDirection() == Vec3(0, 0, 0));
+}
