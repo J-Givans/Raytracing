2c63061ebaed328580c22ce6a6b862f9001a0b53
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-05-22 11:14:51 +0300

    Feature - use randomly generated rays to produce a matte-looking sphere
    This is the first render of a diffuse sphere

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "HittableList.hpp"
 #include "Sphere.hpp"
 #include "Camera.hpp"
+#include "Vec3.hpp"
 
 #include <iostream>
 
@@ -11,7 +12,7 @@ using namespace rt;
 /// \brief Linearly blend white and blue colours
 /// \param[in] ray The ray whose colour at a point is to be computed
 /// \returns A blended colour of white and blue
-Colour rayColour(Ray const& ray, Hittable const& world) noexcept;
+Colour rayColour(Ray const& ray, Hittable const& world, int recursionDepth) noexcept;
 
 int main()
 {
@@ -20,6 +21,7 @@ int main()
     static constexpr int imgWidth {400};
     static constexpr auto imgHeight = static_cast<int>(imgWidth / aspectRatio);
     static constexpr int samplesPerPixel = 100;
+    static constexpr int maxRecursionDepth = 50;
 
     // World
     HittableList world;
@@ -45,7 +47,7 @@ int main()
                 auto v = (j + rand) / (imgHeight - 1);
 
                 Ray ray = cam.getRay(u, v);
-                pixelColour = pixelColour + rayColour(ray, world);
+                pixelColour = pixelColour + rayColour(ray, world, maxRecursionDepth);
             }
 
             writeColour(std::cout, pixelColour, samplesPerPixel);
@@ -56,10 +58,18 @@ int main()
     return EXIT_SUCCESS;
 }
 
-Colour rayColour(Ray const& ray, Hittable const& world) noexcept
+Colour rayColour(Ray const& ray, Hittable const& world, int recursionDepth) noexcept
 {
-    if (HitRecord record; world.hit(ray, 0, infinity, record)) {
-        return 0.5 * (record.normal + Colour(1, 1, 1));
+    HitRecord record; 
+
+    // If we've exceeded the ray bounce limit, no more light is gathered
+    if (recursionDepth <= 0) {
+        return Colour(0, 0, 0);
+    }
+
+    if (world.hit(ray, 0, infinity, record)) {
+        Point3 target = record.point + record.normal + randomInUnitSphere();
+        return 0.5 * rayColour(Ray(record.point, target - record.point), world, recursionDepth - 1);
     }
 
     Vec3 unitDirection = unitVector(ray.getDirection());    // scale the ray direction to unit length
